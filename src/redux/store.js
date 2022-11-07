71cc40df874eea93047f02f7d7df9fa2d9ed5892
Alhajideen <salifunurudeen834@gmail.com>
2022-11-07 17:57:27 -0600

    rename bookAction.js to books.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import bookReducer from './books/bookAction';
+import bookReducer from './books/books';
 
 export const store = configureStore({
   reducer: {
