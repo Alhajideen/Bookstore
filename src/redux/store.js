4cb0197e6a0702961e9311940959428ab274c38f
Alhajideen <salifunurudeen834@gmail.com>
2022-11-07 18:05:40 -0600

    add categories in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
+import catReducer from './categories/categories';
 
 export const store = configureStore({
   reducer: {
     book: bookReducer,
+    categories: catReducer,
   },
 });
