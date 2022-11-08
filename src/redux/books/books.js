f2c02691711bfc5198cdf0c0b16880cdf11242bc
Alhajideen <salifunurudeen834@gmail.com>
2022-11-08 16:22:41 -0600

    implement removeBook action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,21 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = [
-  {
-    id: uuidv4(),
-    title: 'The Return of Ikenga',
-    author: 'Chukwuemeka Ohuka',
-  },
-  {
-    id: uuidv4(),
-    title: 'Our Husband has gone Mad again',
-    author: 'Onoja Victor',
-  },
-  {
-    id: uuidv4(),
-    title: 'Chike and the River',
-    author: 'Chinua Achebe',
-  },
+
 ];
 
 export const bookSlice = createSlice({
