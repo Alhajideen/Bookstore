78556ddb69b169846e5487e70820a9388a498aca
Alhajideen <salifunurudeen834@gmail.com>
2022-11-08 16:28:49 -0600

    dispatch actions for categories

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -3,7 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 function Category() {
-
+  const dispatch = useDispatch();
+  const info = useSelector((state) => state.categories);
+  const construction = info[0];
   return (
     <div>
       <h2>{construction}</h2>
