ae5a9d1e1f2886a4041b2f211658cdf4d8dfa82d
Alhajideen <salifunurudeen834@gmail.com>
2022-11-08 16:23:08 -0600

    add list of books in initialState

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,7 +2,21 @@ import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = [
-
+  {
+    id: uuidv4(),
+    title: 'The Return of Ikenga',
+    author: 'Chukwuemeka Ohuka',
+  },
+  {
+    id: uuidv4(),
+    title: 'Our Husband has gone Mad again',
+    author: 'Onoja Victor',
+  },
+  {
+    id: uuidv4(),
+    title: 'Chike and the River',
+    author: 'Chinua Achebe',
+  },
 ];
 
 export const bookSlice = createSlice({
