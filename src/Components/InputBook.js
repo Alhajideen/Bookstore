04d2db637491224360e023afa2ec25f5862dff39
Alhajideen <salifunurudeen834@gmail.com>
2022-11-08 16:04:54 -0600

    implement add new book feature

--- src/Components/InputBook.js
+++ src/Components/InputBook.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 function InputBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [cat, setCat] = useState('');
+  const dispatch = useDispatch();
+  const handleAdd = () => {
+    const newBook = {
+      id: uuid(),
+      title,
+      author,
+      category: cat,
+    };
+    dispatch(addBook(newBook));
+  };
   return (
     <div>
       <form className="input-book">
         <div className="book-title">
-          <input type="text" placeholder="Book Title" />
+          <input
+            type="text"
+            placeholder="Book Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="book-author">
-          <input type="text" placeholder="Book Author" />
+          <input
+            type="text"
+            placeholder="Book Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <div className="book-cat">
+          <input
+            type="text"
+            placeholder="Book Category"
+            value={cat}
+            onChange={(e) => setCat(e.target.value)}
+          />
         </div>
         <div className="add-btn">
-          <button type="button">Add Book</button>
+          <button type="button" onClick={handleAdd}>
+            Add Book
+          </button>
         </div>
       </form>
     </div>
