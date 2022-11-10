3ba91741a613d74c7f743d529b2d6c1d9ff4750a
Alhajideen <salifunurudeen834@gmail.com>
2022-11-10 17:11:30 -0600

    add other items in a book section

--- src/Components/BookContainer.js
+++ src/Components/BookContainer.js
@@ -22,14 +22,36 @@ function BookContainer() {
     <div>
       <section className="bookContainer">
         {books.map((book) => (
-          <div key={book.id}>
-            <div className="book-details">
-              <Book props={book} />
-            </div>
-            <div className="book-actions">
-              <button type="button" onClick={() => handleRemove(book.id)}>
-                Remove
-              </button>
+          <div className="book-items" key={book.id}>
+            <div className="book-ppt">
+              <div>
+                <div className="book-details">
+                  <h3>{book.category}</h3>
+                  <Book props={book} />
+                </div>
+                <div className="book-actions">
+                  <button type="button">Comments</button>
+                  <span className="border" />
+                  <button type="button" onClick={() => handleRemove(book.id)}>
+                    Remove
+                  </button>
+                  <span className="border" />
+                  <button type="button">Edit</button>
+                </div>
+              </div>
+              <div className="progress-bar">
+                <div className="circular-progress">
+                  <div className="progress-stats">
+                    <p>65%</p>
+                    <p>Completed</p>
+                  </div>
+                </div>
+              </div>
+              <div className="current">
+                <p>CURRENT CHAPTER</p>
+                <p>CUhapter 17</p>
+                <button type="button">UPDATE PROGRESS</button>
+              </div>
             </div>
           </div>
         ))}
